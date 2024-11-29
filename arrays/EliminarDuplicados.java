bc43427c5745cf58bdd440740e767c520ba50a47
kevinchii <kdperezm@ufpso.edu.co>
2024-11-28 21:54:36 -0500

    Elimina los números duplicados de un array cambiandolo por 0

---
+++ arrays/EliminarDuplicados.java
@@ -0,0 +1,34 @@
+package arrays;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class EliminarDuplicados {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] numeros = new int[5];
+        int suma;
+        int cont = 0;
+        for(int i = 0; i<numeros.length;i++){
+            suma = i+1;
+            System.out.println("Ingresa el numero "+suma);
+            numeros[i] = scan.nextInt();
+        }
+        System.out.println("Array inicial: "+Arrays.toString(numeros));
+        for(int j = 0;j<numeros.length;j++){
+            for(int k = 0;k<numeros.length;k++){
+                if (numeros[j]==numeros[k]) {
+                    cont++;
+                }
+                if (cont>1) {
+                    numeros[k] = 0;
+                    cont = 1;
+                }
+            }
+            cont = 0;
+        }
+        System.out.println("Array final: "+Arrays.toString(numeros));
+        scan.close();
+    }
+}
